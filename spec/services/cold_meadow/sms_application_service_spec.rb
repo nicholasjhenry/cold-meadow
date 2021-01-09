4cf33257d56b7bcbf7661fc0375fa1aac0e59f5c
Nicholas Henry <nicholas.henry@gmail.com>
2021-01-09 13:36:11 -0500

    Correct spec name

--- spec/services/cold_meadow/sms_application_service_spec.rb
+++ spec/services/cold_meadow/sms_application_service_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "ColdMeadow::SmsApplicationService" do
+RSpec.describe ColdMeadow::SmsApplicationService do
   it "sends a message" do
     message_params = {
       uuid: "bafb6c01-1171-4f75-b488-c538c5aacd5a",
