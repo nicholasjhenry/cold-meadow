96e9f8c37ebc50d44516deaa6cdad11d5b4b037a
Nicholas Henry <nicholas.henry@gmail.com>
2021-01-09 20:54:37 -0500

    Bulk insert data for better performance

--- app/services/cold_meadow/sms_application_service.rb
+++ app/services/cold_meadow/sms_application_service.rb
@@ -5,10 +5,9 @@ class ColdMeadow::SmsApplicationService
     command = ColdMeadow::SendMessageCommand.new(params)
 
     if command.valid?
-      command.recipients.map do |recipient|
-        message = create_message(recipient, command)
-        ColdMeadow::MessageJob.perform_later(message)
-      end
+      message_data = build_message_data(command)
+      ids = insert_messages!(message_data)
+      process_messages(ids)
     end
 
     command
@@ -16,13 +15,32 @@ class ColdMeadow::SmsApplicationService
 
   private
 
-  def create_message(recipient, command)
-    ColdMeadow::Message.create!(
-      uuid: command.uuid,
-      recipient_phone_number: recipient.phone_number,
-      sender_personal_name: command.sender.personal_name,
-      body: command.body,
-      state: :pending
-    )
+  def build_message_data(command)
+    now = Time.now.utc
+
+    command.recipients.map do |recipient|
+      {
+        uuid: command.uuid,
+        recipient_phone_number: recipient.phone_number,
+        sender_personal_name: command.sender.personal_name,
+        body: command.body,
+        state: :pending,
+        created_at: now,
+        updated_at: now
+      }
+    end
+  end
+
+  def insert_messages!(data)
+    result = ColdMeadow::Message.insert_all!(data)
+    extract_ids(result)
+  end
+
+  def extract_ids(result)
+    result.rows.map { |row| row.first }
+  end
+
+  def process_messages(ids)
+    ids.each { |id| ColdMeadow::MessageJob.perform_later(id) }
   end
 end
