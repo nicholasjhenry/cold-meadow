900a81858d78602ad4e3ea86af17d09b806e42fc
Nicholas Henry <nicholas.henry@gmail.com>
2021-01-10 12:32:33 -0500

    Ensure leaf params are validated independently

--- spec/models/cold_meadow/send_message_command_spec.rb
+++ spec/models/cold_meadow/send_message_command_spec.rb
@@ -2,7 +2,39 @@ require "rails_helper"
 
 RSpec.describe ColdMeadow::SendMessageCommand, type: :model do
   describe "validation" do
-    context "given invalid params" do
+    context "given no params" do
+      it { is_expected.to be_invalid }
+    end
+
+    context "given an invalid root param" do
+      subject do
+        ColdMeadow::SendMessageCommand.new(
+          uuid: "bafb6c01-1171-4f75-b488-c538c5aacd5a",
+          recipients: [
+            { phone_number: "+15141234567" },
+            { phone_number: "+15141238950" }
+          ],
+          sender: { personal_name: "Jane Smith" },
+          body: nil
+        )
+      end
+
+      it { is_expected.to be_invalid }
+    end
+
+    context "given an invalid leaf param" do
+      subject do
+        ColdMeadow::SendMessageCommand.new(
+          uuid: "bafb6c01-1171-4f75-b488-c538c5aacd5a",
+          recipients: [
+            { phone_number: "+15141234567" },
+            { phone_number: "+15141238950" }
+          ],
+          sender: { personal_name: nil },
+          body: nil
+        )
+      end
+
       it { is_expected.to be_invalid }
     end
 
